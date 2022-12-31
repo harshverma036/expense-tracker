2ae7fc32a7c16b8de5cd78a6a6dc0d6dd7bb7b9d
harshverma036 <harshverma0362@gmail.com>
2022-12-31 10:48:12 +0530

    transaction done

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import './transaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Expense Tracker App",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "t1",
+      title: 'New Shoes',
+      amount: 88.98,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "New Tshirt",
+      amount: 699.00,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Tracker'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const <Widget>[
+          Card(
+            elevation: 5,
+            margin: EdgeInsets.all(10.0),
+            child: Text('CHART!'),
+          ),
+          Card(
+            child: Text('LIST OF TRANSACTIONS!!'),
+          ),
+        ],
+      ),
+    );
+  }
+}
